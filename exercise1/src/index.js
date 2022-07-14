5a4a71d9ed7a25644619195009ab0bc7d5e02ab0
Thanh Phong <hoangtu9x1402@gmail.com>
2022-07-14 18:47:06 +0700

    add exercise create table

---
+++ exercise1/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+// [Bài tập] Tạo Element thể hiện bảng thông tin các sinh viên trong lớp học
+
+const students = [
+  {
+    company: 'Alfreds Futterkiste',
+    contact:  'Maria Anders',
+    country: 'Germany'
+  }, {
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico'
+  },
+  {
+    company: 'Ernst Handel',
+    contact: 'Roland Mendel',
+    country: 'Austria'
+  },
+  {
+    company: 'Island Trading',
+    contact: 'Helen Bennett',
+    country: 'UK'
+  },
+  {
+    company: 'Laughing Bacchus Winecellars',
+    contact: 'Yoshi Tannamuri',
+    country: 'Canada'
+  },
+  {
+    company: 'Magazzini Alimentari Riuniti',
+    contact: 'Giovanni Rovelli',
+    country: 'Italy'
+  }
+]
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div className='cotainer'>
+  {students.map(student => (
+    <tr>
+      <td>{student.company}</td>
+      <td>{student.contact}</td>
+      <td>{student.country}</td>
+    </tr>
+  ))}
+  </div>
+);
+
+
